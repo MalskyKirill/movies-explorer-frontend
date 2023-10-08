f38484cacce62da551c1ca4882f39f79795a7095
Kirill Malskiy <sabbat@mail.ru>
2023-10-08 11:10:10 +0300

    После сабмита формы поиска производится валидация. Если в поле не введён текст, выводится ошибка «Нужно ввести ключевое слово».

--- src/components/Movies/Movies.js
+++ src/components/Movies/Movies.js
@@ -1,23 +1,43 @@
-import './Movies.css'
+import './Movies.css';
 import MoviesCardList from '../MoviesCardList/MoviesCardList';
 import SearchForm from '../SearchForm/SearchForm';
 import Preloader from '../Preloader/Preloader';
 import ButtonMoreMovies from '../ButtonMoreMovies/ButtonMoreMovies';
 import { useLocation } from 'react-router-dom';
-
-
+import { useState } from 'react';
 
 function Movies() {
-  const preloader = false
+  const [searchText, setSearchText] = useState(''); //название фильма в инпуте поиска
+  const [isSearchInputEmpty, setIsSearchInputEmpty] = useState(false); //проверка на пустой инпут
+  const [isLoading, setIsLoading] = useState(false); // показать прелоадер
 
-  const location = useLocation()
+  const inputHandler = (evt) => {
+    setSearchText(evt.target.value);
+  }; // изменение инпута
+
+  const handleSubmitMovies = (evt) => {
+    evt.preventDefault();
+    if (searchText.length === 0) {
+      setIsSearchInputEmpty(true);
+      return;
+    }
+
+    setIsSearchInputEmpty(false);
+  }; //отправка формы
+
+  const location = useLocation();
 
   return (
     <main className='content content_main'>
-      <SearchForm />
+      <SearchForm
+        searchText={searchText}
+        isSearchInputEmpty={isSearchInputEmpty}
+        inputHandler={inputHandler}
+        handleSubmitMovies={handleSubmitMovies}
+      />
       <section className='elements' aria-label='Фильмы'>
-      {preloader && <Preloader />}
-        {!preloader && <MoviesCardList />}
+        {isLoading && <Preloader />}
+        {!isLoading && <MoviesCardList />}
       </section>
       {location.pathname === '/movies' ? <ButtonMoreMovies /> : <></>}
     </main>
--- src/components/SearchForm/SearchForm.js
+++ src/components/SearchForm/SearchForm.js
@@ -1,16 +1,19 @@
 import './SearchForm.css';
 import FilterCheckbox from './FilterCheckbox/FilterCheckbox';
 
-function SearchForm() {
+function SearchForm({searchText, isSearchInputEmpty, inputHandler, handleSubmitMovies}) {
   return (
     <section className='search' aria-label='Поиск'>
       <div className='search__wrap'>
-        <form className='search__form' name='search-form'>
+        <form className='search__form' name='search-form' onSubmit={handleSubmitMovies}>
+        {isSearchInputEmpty && <span className='search__error'>{'Нужно ввести ключевое слово'}</span>}
           <input
             type='text'
             className='search__text'
             name='searchText'
             placeholder='Фильм'
+            value={searchText}
+            onChange={inputHandler}
           />
           <button className='search__button' type='submit'></button>
         </form>
