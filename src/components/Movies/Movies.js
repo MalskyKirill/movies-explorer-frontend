50f919670a7465f7121064185f05760f84c05311
Kirill Malskiy <sabbat@mail.ru>
2023-10-09 22:24:56 +0300

    добавил логику добавления карточек на страницу в зависимости от размера экрана и дополнительной отрисовки карточек при клике по кнопке еще

--- src/components/Movies/Movies.js
+++ src/components/Movies/Movies.js
@@ -14,34 +14,37 @@ function Movies() {
   const [searchText, setSearchText] = useState(''); //название фильма в инпуте поиска
   const [isSearchInputEmpty, setIsSearchInputEmpty] = useState(false); //проверка на пустой инпут
   const [isLoading, setIsLoading] = useState(false); // показать прелоадер
-  const [isShowMoreButton, setIsShowMoreButton] = useState(true); //показать кнопку еще
+  // const [isShowMoreButton, setIsShowMoreButton] = useState(true); //показать кнопку еще
   const [isSearchMovies, setIsSearchMovies] = useState(false); //проверка на пустой searchMovies
   const [isShot, setIsShot] = useState(false); // проверка на короткометражку
   const [isApiError, setIsApiError] = useState(false); //проверка на ошибку при запросе
 
+  const [windowInnerWidth, setWindowInnerWidth] = useState(window.innerWidth); // текущий размер экрана
+  const [cardsCount, setCardsCount] = useState(5); //количество карточек изначально на странице
+  const [moreCardsCount, setMoreCardsCount] = useState(0); //добавляемое количество карточек
+
   const location = useLocation();
 
+  // изменение инпута
   const inputHandler = (evt) => {
     setSearchText(evt.target.value);
-  }; // изменение инпута
+  };
 
+  //отправка формы
   const handleSubmitMovies = (evt) => {
     evt.preventDefault();
     if (searchText.length === 0) {
       setIsSearchInputEmpty(true);
       return;
     }
-
     setIsSearchInputEmpty(false);
 
-    if (searchMovies.length !== 0) {
-      setIsSearchMovies(true);
-    } else {
-      setIsSearchMovies(false);
-    }
+    searchMovies.length !== 0
+      ? setIsSearchMovies(true)
+      : setIsSearchMovies(false);
 
     getSourceArrayMovies(searchText);
-  }; //отправка формы
+  };
 
   //получаем массив фильмов со стороннего Api и сохраняем его в lokalStorege
   const getSourceArrayMovies = (searchText) => {
@@ -62,8 +65,6 @@ function Movies() {
         })
         .finally(() => setIsLoading(false));
     } else {
-      console.log(searchText, movies);
-
       filterMovies(searchText, movies);
     }
   };
@@ -79,19 +80,54 @@ function Movies() {
     setSearchMovies(filter);
   }, []);
 
+  // определение короткометражки
   const shotMovies = (movie) => {
     return movie.duration <= 40 ? setIsShot(true) : setIsShot(false);
-  }; // определение короткометражки
+  };
+
+  // проверяем количество карточек в зависимости от разммера экрана
+  const checkCardsCount = () => {
+    if (windowInnerWidth >= 1280) {
+      setCardsCount(16);
+      setMoreCardsCount(4);
+    } else if (windowInnerWidth >= 1044) {
+      setCardsCount(9);
+      setMoreCardsCount(3);
+    } else if (windowInnerWidth >= 768) {
+      setCardsCount(8);
+      setMoreCardsCount(2);
+    } else if (windowInnerWidth <= 619) {
+      setCardsCount(5);
+      setMoreCardsCount(2);
+    }
+  };
+
+  //устанавливает значение размера экрана
+  function handleResize() {
+    setWindowInnerWidth(window.innerWidth);
+  }
+
+  // добавляет карточки при нажании на кнопку еще
+  function handleLoadMoreCards() {
+    checkCardsCount();
+    setCardsCount(cardsCount + moreCardsCount);
+  }
 
   useEffect(() => {
     if (localStorage.moviesData && localStorage.searchText) {
       const movies = JSON.parse(localStorage.moviesData);
       const search = JSON.parse(localStorage.searchText);
-      //setIsLoading(false);
       setMovies(movies);
       filterMovies(search, movies);
     }
   }, [filterMovies]);
+
+  useEffect(() => {
+    checkCardsCount();
+    handleResize();
+    window.addEventListener('resize', handleResize);
+    return () => window.removeEventListener('resize', handleResize);
+  }, [windowInnerWidth]);
 
   return (
     <main className='content content_main'>
@@ -106,12 +142,14 @@ function Movies() {
       <section className='elements' aria-label='Фильмы'>
         {isLoading && <Preloader />}
         {!isLoading && (
-          <MoviesCardList
-            searchMovies={searchMovies}
-          />
+          <MoviesCardList searchMovies={searchMovies} cardsCount={cardsCount} />
         )}
       </section>
-      {location.pathname === '/movies' && isShowMoreButton ? <ButtonMoreMovies /> : <></>}
+      {searchMovies.length > cardsCount ? (
+        <ButtonMoreMovies handleLoadMoreCards={handleLoadMoreCards} />
+      ) : (
+        <></>
+      )}
     </main>
   );
 }
--- src/components/MoviesCardList/MoviesCardList.js
+++ src/components/MoviesCardList/MoviesCardList.js
@@ -2,15 +2,17 @@ import './MoviesCardList.css';
 import MoviesCard from '../MoviesCard/MoviesCard';
 import { useState } from 'react';
 
-function MoviesCardList({searchMovies}) {
+function MoviesCardList({searchMovies, cardsCount}) {
 
   const onCardDelete = (id) => {
     // setCards((cards) => cards.filter((card) => card.id !== id));
   };
 
+  console.log(cardsCount)
+
   return (
     <ul className='movies'>
-      {searchMovies.map((card) => (
+      {searchMovies.slice(0, cardsCount).map((card) => (
         <MoviesCard
           movies={card}
           key={card.id}
