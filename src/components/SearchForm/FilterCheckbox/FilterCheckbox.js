214e8643aefeaf5cfc052fdf742112f767c41097
Kirill Malskiy <sabbat@mail.ru>
2023-10-16 12:05:23 +0300

    на странице saved-movies реализовал возможность переключения чекбокса при пустой строке поиска

--- src/components/SearchForm/FilterCheckbox/FilterCheckbox.js
+++ src/components/SearchForm/FilterCheckbox/FilterCheckbox.js
@@ -1,9 +1,25 @@
-import './FilterCheckbox.css'
+import { useLocation } from 'react-router-dom';
+import './FilterCheckbox.css';
 
-function FilterCheckbox({handleShort, searchText}) {
+function FilterCheckbox({ handleShort, searchText }) {
+  const location = useLocation();
+
   return (
     <label className='search__switch'>
-      <input className='search__checkbox-input' type='checkbox' onChange={handleShort} disabled={searchText.length === 0}/>
+      {location.pathname === '/movies' ? (
+        <input
+          className='search__checkbox-input'
+          type='checkbox'
+          onChange={handleShort}
+          disabled={searchText.length === 0}
+        />
+      ) : (
+        <input
+          className='search__checkbox-input'
+          type='checkbox'
+          onChange={handleShort}
+        />
+      )}
       <div className='search__checkbox-custom'></div>
       <p className='search__text-shorts'>Короткометражки</p>
     </label>
