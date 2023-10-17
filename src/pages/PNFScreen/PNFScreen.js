bb205aec5575883986f04d649916170e45d8038b
Kirill Malskiy <sabbat@mail.ru>
2023-10-17 19:18:38 +0300

    поправил кнопку назад на pnf

--- src/pages/PNFScreen/PNFScreen.js
+++ src/pages/PNFScreen/PNFScreen.js
@@ -1,14 +1,16 @@
 import { Link } from 'react-router-dom';
+
 import './PNFScreen.css';
 
 function PNFScreen() {
+
   return (
     <>
       <main className='content content_main'>
         <section className='page-not-found'>
           <h1 className='page-not-found__title'>404</h1>
           <p className='page-not-found__subtitle'>Страница не найдена</p>
-          <Link to='/' className='page-not-found__link'>
+          <Link to={-1} className='page-not-found__link'>
             Назад
           </Link>
         </section>
